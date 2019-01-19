d84d5c18132d3866de42a4426991e3b016dd41b9
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2019-01-20 04:16:51 +0500

    User can update profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,8 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
+  # before_action :update_sanitized_params, if: :devise_controller?
 
   # GET /resource/sign_up
   # def new
@@ -20,9 +21,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  def update
-    super
-  end
+  # def update
+  #   super
+  # end
 
   # DELETE /resource
   # def destroy
@@ -39,10 +40,29 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
-  
+
+  def update_resource(resource, params)
+    if params[:current_password].blank?
+     resource.update_without_password(params.except(:current_password))
+    else
+      resource.update_with_password(params)
+    end
+  end
+
+  # redirect user to edit profile path
   def after_update_path_for(resource)
     edit_user_registration_path(resource)
   end
+  
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update,keys: [:first_name, :last_name])
+  end
+
+  # def update_sanitized_params
+  #     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation, :firstname, :lastname, :displayname)}
+  #     devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)}
+  #   end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
