4a8b3a445d2f43671037d0aba22e8e42e577d8aa
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2019-01-24 21:07:03 +0500

    Dashboard Basic for Entry

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,32 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Latest Entries" do
+          table do
+            thead do
+                tr do
+                    th "Id"
+                    th "Question"
+                    th "Answer"
+                    th "By user"
+                end
+            end
+
+            tbody do
+                Entry.latest(5).map do |t|
+                    tr do
+                        td link_to(t.id, admin_entry_path(t))
+                        td t.question.question
+                        td t.answer
+                        td link_to(t.user.email, admin_user_path(t.user))
+                    end
+                end
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,8 @@
 class Entry < ApplicationRecord
   belongs_to :user
   belongs_to :question
+
+  def self.latest n
+    self.last(n).reverse # last 5 records in descending order
+  end
 end
